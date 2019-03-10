9d6e175754b505ac0277248285d1957b849c7fba
kingdaro <kingdaro@gmail.com>
2019-03-10 15:31:48 -0500

    more leak-less image loading

--- src/helpers/loadImage.ts
+++ src/helpers/loadImage.ts
@@ -1,7 +1,20 @@
 export const loadImage = (source: string) =>
   new Promise<HTMLImageElement>((resolve, reject) => {
     const image = new Image()
+
+    const handleLoad = () => {
+      resolve(image)
+      image.removeEventListener("load", handleLoad)
+      image.removeEventListener("error", handleError)
+    }
+
+    const handleError = () => {
+      reject(`Failed to load image from url "${source}"`)
+      image.removeEventListener("load", handleLoad)
+      image.removeEventListener("error", handleError)
+    }
+
+    image.addEventListener("load", handleLoad)
+    image.addEventListener("error", handleError)
     image.src = source
-    image.onload = () => resolve(image)
-    image.onerror = reject
   })
